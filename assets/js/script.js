429936ddd18ee4045b00d191cf7b2eafccdff50b
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-06-10 14:30:06 +0200

    adds static compareAge method

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ class User {
     this.firstName = fisrtName;
     this.lastName = lastName;
     this.age = age;
-    this.location = locations;
+    this.location = location;
   }
 
   compareAge(ageToCompare) {
@@ -13,6 +13,16 @@ class User {
       console.log(`${ageToCompare.firstName} è più grande di ${this.firstName}`);
     } else {
       console.log(`${this.firstName} e ${ageToCompare.firstName} hanno la stessa età`);
+    }
+  }
+
+  static compareAgeStatic(user1, user2) {
+    if (user1.age > user2.age) {
+      console.log(`${user1.firstName} è più grande di ${user2.firstName}`);
+    } else if (user1.age < user2.age) {
+      console.log(`${user2.firstName} è più grande di ${user1.firstName}`);
+    } else {
+      console.log(`${user1.firstName} e ${user2.firstName} hanno la stessa età`);
     }
   }
 }
