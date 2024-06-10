b5d04ecff5202db2343fd23081ba3a7116ad3019
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-06-10 14:26:33 +0200

    adds compareAge method

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,14 @@ class User {
     this.age = age;
     this.location = locations;
   }
+
+  compareAge(ageToCompare) {
+    if (this.age > ageToCompare.age) {
+      console.log(`${this.firstName} è più grande di ${ageToCompare.firstName}`);
+    } else if (this.age < ageToCompare.age) {
+      console.log(`${ageToCompare.firstName} è più grande di ${this.firstName}`);
+    } else {
+      console.log(`${this.firstName} e ${ageToCompare.firstName} hanno la stessa età`);
+    }
+  }
 }
