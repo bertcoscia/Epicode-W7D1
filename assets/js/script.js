4ee14809754b13f10f4dd1b3a29c28877ecc5c5f
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-06-10 14:46:54 +0200

    instantiates 4 objects and tests methods

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 class User {
-  constructor(fisrtName, lastName, age, location) {
-    this.firstName = fisrtName;
+  constructor(firstName, lastName, age, location) {
+    this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
     this.location = location;
@@ -8,21 +8,39 @@ class User {
 
   compareAge(ageToCompare) {
     if (this.age > ageToCompare.age) {
-      console.log(`${this.firstName} è più grande di ${ageToCompare.firstName}`);
+      console.log(`${this.firstName} (${this.age}) è più grande di ${ageToCompare.firstName} (${ageToCompare.age})`);
     } else if (this.age < ageToCompare.age) {
-      console.log(`${ageToCompare.firstName} è più grande di ${this.firstName}`);
+      console.log(`${this.firstName} (${this.age}) è più giovane di ${ageToCompare.firstName} (${ageToCompare.age})`);
     } else {
-      console.log(`${this.firstName} e ${ageToCompare.firstName} hanno la stessa età`);
+      console.log(`${this.firstName} (${this.age}) e ${ageToCompare.firstName} (${ageToCompare.age}) hanno la stessa età`);
     }
   }
 
   static compareAgeStatic(user1, user2) {
     if (user1.age > user2.age) {
-      console.log(`${user1.firstName} è più grande di ${user2.firstName}`);
+      console.log(`STATIC - ${user1.firstName} (${user1.age}) è più grande di ${user2.firstName} (${user2.age})`);
     } else if (user1.age < user2.age) {
-      console.log(`${user2.firstName} è più grande di ${user1.firstName}`);
+      console.log(`STATIC - ${user1.firstName} (${user1.age}) è più giovane di ${user2.firstName} (${user2.age})`);
     } else {
-      console.log(`${user1.firstName} e ${user2.firstName} hanno la stessa età`);
+      console.log(`STATIC - ${user1.firstName} (${user1.age}) e ${user2.firstName} (${user2.age}) hanno la stessa età`);
     }
   }
 }
+
+const samviseGamgee = new User("Samvise", "Gamgee", 38, "The Shire");
+const theodenEdnew = new User("Theoden", "Ednew", 71, "Rohan");
+const gandalfTheGrey = new User("Gandalf", "The Grey", 2000, "Minas Tirith");
+const sarumanTheWhite = new User("Saruman", "The White", 2000, "Isengard");
+
+console.log(samviseGamgee);
+console.log(theodenEdnew);
+console.log(gandalfTheGrey);
+console.log(sarumanTheWhite);
+
+samviseGamgee.compareAge(theodenEdnew);
+theodenEdnew.compareAge(samviseGamgee);
+gandalfTheGrey.compareAge(sarumanTheWhite);
+
+User.compareAgeStatic(samviseGamgee, theodenEdnew);
+User.compareAgeStatic(theodenEdnew, samviseGamgee);
+User.compareAgeStatic(gandalfTheGrey, sarumanTheWhite);
